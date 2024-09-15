9b5cbcd1d87a65629dc601851070f5c6e97342ef
Rafa <mirandarafa010@gmail.com>
2024-09-15 12:00:00 -0600

    Añadir archivo_dia_1.py en 2024-09-15T12:00:00

---
+++ archivo_dia_1.py
@@ -0,0 +1,8 @@
+# Caracteres especiales
+# Rafael Miranda Jimenez
+
+print('Hola \nMundo') # \n salto de linea.
+print('\tPython \t\tes genial') # \t agrega un tabulador.
+print('Juan\' "Perez"')
+print('Karla \"Gomez"')
+print('Caracter \\ diagonal invertida')
