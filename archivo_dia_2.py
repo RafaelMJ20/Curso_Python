679eb9398692ba2088bc65ecc5b711c62e3d0497
Rafa <mirandarafa010@gmail.com>
2024-09-16 12:00:00 -0600

    Añadir archivo_dia_2.py en 2024-09-16T12:00:00

---
+++ archivo_dia_2.py
@@ -0,0 +1,12 @@
+# Concatenacion de cadenas
+# Rafael Miranda Jimenez
+
+cadena1 = 'Hola'
+cadena2 = 'Mundo'
+concatenacion = cadena1 +  ' ' + cadena2
+
+print(concatenacion)
+
+# Utilizando el metodo join
+concatenacion = ''.join([cadena1, ' ', cadena2])
+print(concatenacion)
