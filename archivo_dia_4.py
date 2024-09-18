a55487df44e468521f916af363b7b1adcb2e1867
Rafa <mirandarafa010@gmail.com>
2024-09-18 12:00:00 -0600

    Añadir archivo_dia_4.py en 2024-09-18T12:00:00

---
+++ archivo_dia_4.py
@@ -0,0 +1,14 @@
+# Programa para presentación
+# Rafael Miranda Jimenez
+
+print("Nombre: Rafael Miranda Jimenez")
+print("Edad: 21")
+print("Pais: México")
+
+name = "Rafael Miranda Jimenez"
+age = 21
+country = "México"
+
+print("Nombre: ", name)
+print("Edad: ", age)
+print("Pais: ", country)
