0885cc9cbfc62ce1e658c2e89bfe4c9928074bab
Rafa <mirandarafa010@gmail.com>
2024-09-19 12:00:00 -0600

    Añadir archivo_dia_5.py en 2024-09-19T12:00:00

---
+++ archivo_dia_5.py
@@ -0,0 +1,27 @@
+# Ejercicio de reserva de hotel
+# Rafael Miranda Jimenez
+
+# Definir variables
+nombre_cliente = 'Pedro Sanchez'
+dias_estancia = 10
+tarifa_diaria = 1300
+vista_mar = False
+
+# Mostrar el detalle de la reserva
+print('*** Sistema de Reserva de Hoteles ***')
+print('Cliente: ', nombre_cliente)
+print('Días de estancia: ', dias_estancia)
+print('Tarifa diaria: ', tarifa_diaria)
+print('Habitación con vista al mar: ', vista_mar)
+
+# Realizar modificaciones
+dias_estancia = 5
+tarifa_diaria = 1000
+vista_mar = True
+
+# Mostrar el detalle ya modificado
+print()
+print('Cliente: ', nombre_cliente)
+print('Días de estancia: ', dias_estancia)
+print('Tarifa diaria: ', tarifa_diaria)
+print('Habitación con vista al mar: ', vista_mar)
