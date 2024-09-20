fb85da5f709d184c91112cdbbc0863ab6fa04844
Rafa <mirandarafa010@gmail.com>
2024-09-20 12:00:00 -0600

    Añadir archivo_dia_6.py en 2024-09-20T12:00:00

---
+++ archivo_dia_6.py
@@ -0,0 +1,26 @@
+# Ejercicio de tienda online
+# Rafael Miranda Jimenez
+
+# Declarar variables
+producto = 'Laptop ASUS Rog Tuf A15'
+precio_producto = '27999.99'
+cantidad = 13
+disponibilidad = True
+
+# Mostrar el detalle
+print('*** SISTEMA DE TIENDA ONLINE ***')
+print('Producto: ', producto)
+print('Precio: $', precio_producto)
+print('Cantidad inventario: ', cantidad)
+print('Disponible: ', disponibilidad)
+
+# Modificar variables
+precio_producto = 15000
+disponibilidad = False
+
+# Mostrar el detalle modificado
+print()
+print('Producto: ', producto)
+print('Precio: $', precio_producto)
+print('Cantidad inventario: ', cantidad)
+print('Disponible: ', disponibilidad)
