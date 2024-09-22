a6bead7eff9d14870afff9da3fa23c727aaea78a
Rafa <mirandarafa010@gmail.com>
2024-09-22 12:00:00 -0600

    Añadir archivo_dia_8.py en 2024-09-22T12:00:00

---
+++ archivo_dia_8.py
@@ -0,0 +1,13 @@
+# Manejo de indice en una cadena
+# Rafael Miranda Jimenez
+
+cadena1 = 'Hola Mundo'
+print(cadena1)
+
+# Recuperar el primer caracter
+primer_caracter = cadena1[0] # Recupera 'H'
+print(primer_caracter)
+
+ultimo_caracter = cadena1[9] # Recupera 'o'
+print(ultimo_caracter)
+
