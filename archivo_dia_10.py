7e4b0e91e6f508d99b07a4c60348fd4f783effc4
Rafa <mirandarafa010@gmail.com>
2024-09-24 12:00:00 -0600

    Añadir archivo_dia_10.py en 2024-09-24T12:00:00

---
+++ archivo_dia_10.py
@@ -0,0 +1,13 @@
+# Cadenas en Python
+# Rafael Miranda Jimenez
+
+cadena1 = 'Hola Mundo'
+cadena1 = 'Adios'
+cadena2 = 'Python es genial'
+cadena3 = '''Este es un ejemplo
+de multiple lineas
+en una cadena'''
+
+print(cadena1)
+print(cadena2)
+print(cadena3)
