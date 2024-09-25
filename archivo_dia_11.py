ec80f945282f494aa7b5d829320bc278f52b4a56
Rafa <mirandarafa010@gmail.com>
2024-09-25 12:00:00 -0600

    Añadir archivo_dia_11.py en 2024-09-25T12:00:00

---
+++ archivo_dia_11.py
@@ -0,0 +1,24 @@
+# Regla y convenciones en nombres de variables
+# Rafael Miranda Jimenez
+
+# Ejemplos de reglas estrictas
+nombre_usuario = 'Rafa Miranda'
+# 1nombre_usuario = 'Pedro Perez'
+
+# No podemos usar palabras reservadas
+
+# class = 'Mi clase'
+klass = 'Mi clase'
+
+# Sensible a mayúsculas y minúsculas
+nombre = 'Pedro'
+Nombre = 'Olivia'
+print(nombre)
+print(Nombre)
+
+# snake case
+nombre_completo = 'Rafael Miranda'
+
+# prefijos y sufijos
+es_casado = True
+nombre_txt = 'archivo.txt'
