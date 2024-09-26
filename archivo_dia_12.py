ca65d9cba6febf0160819c6238c0fbc659b1e985
Rafa <mirandarafa010@gmail.com>
2024-09-26 12:00:00 -0600

    Añadir archivo_dia_12.py en 2024-09-26T12:00:00

---
+++ archivo_dia_12.py
@@ -0,0 +1,22 @@
+# Ejemplo de tipos de datos en Python.
+# Rafael Miranda Jimenez
+
+# Entero
+edad = 21
+print('Edad: ', edad)
+
+# Número con punto flotante
+altura = 1.54
+print('Altura: ', altura)
+
+# Cadena de texto
+nombre = 'Ana'
+print('Nombre: ', nombre)
+
+# Tipo boolean
+es_estudiante = False
+print('Es estudiante? ', es_estudiante)
+
+# None: ausencia de valor
+direccion = None
+print('Dirección: ', direccion)
