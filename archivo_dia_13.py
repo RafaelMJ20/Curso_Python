509846f8b680ef5c488f2c53eda043a656136cd8
Rafa <mirandarafa010@gmail.com>
2024-09-27 12:00:00 -0600

    Añadir archivo_dia_13.py en 2024-09-27T12:00:00

---
+++ archivo_dia_13.py
@@ -0,0 +1,31 @@
+# Variables en Python
+# Rafael Miranda Jimenez
+
+# Declaración e inicialización de variables.
+
+edad = 21
+altura = 1.55
+pais = 'México'
+
+# Acceder a las variables
+print('Valores iniciales: ')
+print('Edad: ', edad)
+print('Altura: ', altura)
+print('Pais: ', pais)
+
+# Modificar el valor de una variable
+edad = 30
+altura = 1.60
+
+# Acceder a las variables
+print('Valores modificados: ')
+print('Edad: ', edad)
+print('Altura: ', altura)
+print('Pais: ', pais)
+
+# En python el tipo es dinamico
+edad = 'treinta'
+print('Edad: ', edad)
+
+
+
